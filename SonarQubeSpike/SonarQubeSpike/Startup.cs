89114e7e46be2dcb5889548cc1cb11b728d486e0
Phan Thuc Anh <phan.thuc.anh@ansarada.com>
2017-08-08 11:33:42 +0700

    Initialize project

---
+++ SonarQubeSpike/SonarQubeSpike/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SonarQubeSpike.Startup))]
+namespace SonarQubeSpike
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
